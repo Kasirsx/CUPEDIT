f5997c7ec917c711e80f93c194b1e28841b4f6ac
Hani-Kanakri <knakri01@gmail.com>
2024-06-02 19:09:37 +0300

    add command to (1) (MergeVideosCubit)

--- lib/core/constants/commands.dart
+++ lib/core/constants/commands.dart
@@ -5,18 +5,7 @@ import 'package:path_provider/path_provider.dart';
 import '/core/utils/shared_storage.dart';
 
 class Commands {
-  static Future<String?> outPut() async {
-    final Directory tempDir = await getTemporaryDirectory();
-    final Directory appDocumentsDir = await getApplicationDocumentsDirectory();
-    final Directory? downloadsDir = await getDownloadsDirectory();
-    final videoName = now().toString();
-    return "${downloadsDir!.path}/merge$videoName.mp4";
-  }
 
-  static String now() {
-    final DateTime now = DateTime.now();
-    return "${now.year}_${now.month}_${now.day}_${now.hour}_${now.minute}";
-  }
 
   static String mergeVideosCommand(
       {String? inputPath1, String? inputPath2, String? outPath}) {
